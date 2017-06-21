aa4ded117c4c4daaec3b154b04a242085f7627a7
Dawid Pośliński <dposlinski@gmail.com>
2017-06-22 00:23:15 +0200

    Ember version checker component

--- app/components/commands/ember-version/component.js
+++ app/components/commands/ember-version/component.js
@@ -3,8 +3,13 @@ const { Component, inject, computed } = Ember;
 
 export default Component.extend({
   terminal: inject.service(),
+  projectPath: '',
+  rootPath: '',
 
-  version: computed(function() {
-    return this.get('terminal').execute('ember -v');
+  // TODO add some caching later on - it is very heavy operation
+  version: computed('projectPath', 'rootPath', function() {
+    let fullPath = `${this.get('rootPath')}/${this.get('projectPath')}`;
+    let versionCommand = 'npm list ember-cli | grep ember-cli@ | cut -d"@" -f2';
+    return this.get('terminal').execute(`cd ${fullPath} && ${versionCommand}`);
   })
 });
